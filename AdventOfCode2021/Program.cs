1467c5fff67686c0dd92e67e60a1cb0422cf5953
kappertv <vincent.kappert@gmail.com>
2021-12-01 21:57:46 +0100

    day 1

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AdventOfCode2021.Day1Sonardepth;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1");
+            new AnalyseSonarDepth().CountSonarIncreases();
+        }
+    }
+}
