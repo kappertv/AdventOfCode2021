5c68131125140e7ec4a70c4c96150c85f6f5ccfc
kappertv <vincent.kappert@gmail.com>
2021-12-01 22:14:28 +0100

    initial

---
+++ AdventOfCode2021/Day1 - Sonardepth/AnalyseSonarDepth.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Day1Sonardepth
+{
+    public class AnalyseSonarDepth
+    {
+        public AnalyseSonarDepth()
+        {
+            
+        }
+
+        public long CountSonarIncreases()
+        {
+            var lines = File.ReadAllLines("Day1 - Sonardepth//input.txt");
+            var sonarDepths = lines.Select(s => long.Parse(s.Trim()));
+            Console.WriteLine("Total lines:" + sonarDepths.Count());
+            var slidingWindows = GetSlidingWindows(sonarDepths);
+            Console.WriteLine("Total windows:" + slidingWindows.Count());
+            var result =  CountNumberOfIncreases(slidingWindows);
+            Console.WriteLine("Increased lines:" + result);
+            return result;
+        }
+
+        private IEnumerable<long> GetSlidingWindows(IEnumerable<long> sonarDepths)
+        {
+            var sonarDepthArray = sonarDepths.ToArray();
+            var result = new List<long>();
+            for (int i=0; i<sonarDepthArray.Length - 2; i++)
+            {
+                var window = sonarDepthArray[i] + sonarDepthArray[i + 1] + sonarDepthArray[i + 2];
+                result.Add(window);
+            }
+            return result;
+        }
+
+        private long CountNumberOfIncreases(IEnumerable<long> sonarDepths)
+        {
+            var result = 0;
+            var previous = long.MaxValue;
+            foreach (var sd in sonarDepths)
+            {
+                if (sd > previous)
+                {
+                    result++;
+                }
+                previous = sd;
+            }
+            return result;
+        }
+    }
+}
