945b29bd8db59764dc21d1c51af13ba0fe87fa0f
kappertv <vincent.kappert@gmail.com>
2021-12-07 09:01:40 +0100

    Day 7

---
+++ AdventOfCode2021/Day 7/CrabFuelCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2021.Day7
+{
+    public class CrabFuelCalculator
+    {
+        int[] crabposition;
+        int max;
+        int min;
+        public CrabFuelCalculator(string line)
+        {
+            var parts = line.Split(",");
+            var crabpositions = parts.Select(s => int.Parse(s)).ToArray();
+            crabposition = crabpositions;
+            max = crabposition.Max();
+            min = crabposition.Min();
+        }
+
+        public long[] GetCheapestPositionToAllign(bool increasingconsumption)
+        {
+
+            long[] lowestFuelAtPosition = new long[2];
+            lowestFuelAtPosition[0] = long.MaxValue; //fuel
+            lowestFuelAtPosition[1] = 0; // position
+
+
+            for (int i = min; i <= max; i++)
+            {
+                var neededFuel = GetNeededFuelForPosition(i, increasingconsumption);
+                if (lowestFuelAtPosition[0] == neededFuel) throw new Exception("equal distance");
+                if (lowestFuelAtPosition[0] > neededFuel)
+                {
+                    lowestFuelAtPosition[0] = neededFuel;
+                    lowestFuelAtPosition[1] = i;
+                }
+            }
+
+            return lowestFuelAtPosition;
+        }
+
+        private int GetIncreasingFuel(int neededFuel)
+        {
+            var result = 0;
+            for (int i = 1; i <= neededFuel; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+
+        private long GetNeededFuelForPosition(int position, bool inc)
+        {
+            long result = 0;
+            for (int i = 0; i < crabposition.Length;i++)
+            {
+                var distance = Math.Abs(crabposition[i] - position);
+                if (inc)
+                {
+                    distance = GetIncreasingFuel(distance);
+                }
+                result += distance;
+            }
+            return result;
+        }
+    }
+}
---
+++ Tests/Day 7/CrabFuelCalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode2021.Day7;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Tests.Day7
+{
+    [TestClass]
+    public class CrabFuelCalculatorTests
+    {
+        CrabFuelCalculator _sut;
+        CrabFuelCalculator _sutmyinput;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var line = File.ReadAllLines("Day 7//sampleinput.txt").Single();
+            _sut = new CrabFuelCalculator(line);
+
+            var myline = File.ReadAllLines("Day 7//myinput.txt").Single();
+            _sutmyinput = new CrabFuelCalculator(myline);
+        }
+
+        [TestMethod]
+        public void ResultOfSampleInputIsPosition2AndConsumption37()
+        {
+            var result = _sut.GetCheapestPositionToAllign(false);
+
+            result[0].ShouldBe(37);
+            result[1].ShouldBe(2);
+        }
+
+        [TestMethod]
+        public void ResultOfSampleIncreasingConsumption()
+        {
+            var result = _sut.GetCheapestPositionToAllign(true);
+
+            result[0].ShouldBe(168);
+            result[1].ShouldBe(5);
+        }
+
+        [TestMethod]
+        public void ResultOfMyInpuInput()
+        {
+            var result = _sutmyinput.GetCheapestPositionToAllign(true);
+
+            result[0].ShouldBe(0);
+            result[1].ShouldBe(2);
+        }
+    }
+}
