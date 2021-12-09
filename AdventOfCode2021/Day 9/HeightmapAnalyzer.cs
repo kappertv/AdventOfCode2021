cd87414f0f4ddab41c309b0e9053a4fa718ec6c2
kappertv <vincent.kappert@gmail.com>
2021-12-09 22:09:46 +0100

    Day 9 - par 1

---
+++ AdventOfCode2021/Day 9/HeightmapAnalyzer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Day9
+{
+    public class HeightmapAnalyzer
+    {
+        public int[,] heightmap;
+        private int maxY;
+        private int maxX;
+        public HeightmapAnalyzer(string[] lines)
+        {
+            maxX = lines.First().Length;
+            maxY = lines.Length;
+
+            heightmap = new int[maxY, maxX];
+            for (int y=0;y<maxY;y++)
+            {
+                for (int x=0;x<maxX;x++)
+                {
+                    heightmap[y, x] = int.Parse(lines[y][x].ToString());
+                }
+            }
+        }
+
+        public int[] GetLowPoints()
+        {
+            var result = new List<int>();
+            for (int y=0;y<maxY;y++)
+            {
+                for (int x=0;x<maxX;x++)
+                {
+                    var p = heightmap[y, x];
+                    var surrounding = GetSurroundingHeights(y, x);
+                    if (p < surrounding.Min())
+                    {
+                        result.Add(p);
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private int[] GetSurroundingHeights(int y, int x)
+        {
+            var result = new List<int>();
+
+            if (y != 0)
+            {
+                result.Add(heightmap[y - 1, x]);
+            }
+            if (y != (maxY-1))
+            {
+                result.Add(heightmap[y + 1, x]);
+            }
+            if (x != (maxX-1))
+            {
+                result.Add(heightmap[y, x + 1]);
+            }
+            if (x != 0)
+            {
+                result.Add(heightmap[y, x - 1]);
+            }
+            return result.ToArray();
+        }
+
+        public int GetSumOfRiskLevels()
+        {
+            var lowpoints = GetLowPoints();
+            return lowpoints.Sum() + lowpoints.Count();
+        }
+    }
+}
---
+++ Tests/Day 9/HeightmapAnalzyerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using AdventOfCode2021.Day9;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Tests.Day9
+{
+    [TestClass]
+    public class HeightmapAnalyzerTest
+    {
+        HeightmapAnalyzer _sut;
+        HeightmapAnalyzer _sutmyinput;
+        [TestInitialize]
+        public void Initialize()
+        {
+            var line = File.ReadAllLines("Day 9//sampleinput.txt");
+            _sut = new HeightmapAnalyzer(line);
+
+            var myline = File.ReadAllLines("Day 9//myinput.txt");
+            _sutmyinput = new HeightmapAnalyzer(myline);
+        }
+
+        [TestMethod]
+        public void GetSumOfRiskLevels()
+        {
+            var result = _sut.GetSumOfRiskLevels();
+
+            result.ShouldBe(15);
+        }
+
+        [TestMethod]
+        public void GetSumOfRiskLevelsMyInput()
+        {
+            var result = _sutmyinput.GetSumOfRiskLevels();
+
+            result.ShouldBe(15);
+        }
+    }
+}
